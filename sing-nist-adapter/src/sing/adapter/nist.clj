5cd0fecbe4e17ebf1c4dd3db6046308eb90b01cc
Ruiyun <ruiyun.wen@gmail.com>
2013-04-01 21:35:44 +0800

    working in prograess.

--- sing-nist-adapter/src/sing/adapter/nist.clj
+++ sing-nist-adapter/src/sing/adapter/nist.clj
@@ -1,12 +1,14 @@
 (ns sing.adapter.nist
   "Adapter for the nist implemention of JAIN."
   (:refer-clojure :exclude [replace])
-  (:require [clojure.string :refer [upper-case replace]])
+  (:require [clojure.string :refer [upper-case lower-case replace join]])
   (:import [java.util Properties]
-           [javax.sip SipFactory SipStack SipProvider SipListener]
+           [javax.sip SipFactory SipStack SipProvider SipListener ListeningPoint]
            [javax.sip.address AddressFactory]
-           [javax.sip.message MessageFactory]
-           [javax.sip.header HeaderFactory]))
+           [javax.sip.message MessageFactory Request Response]
+           [javax.sip.header HeaderFactory ContentTypeHeader]
+           [gov.nist.javax.sip.message SIPRequest]
+           [gov.nist.javax.sip.header SIPHeader]))
 
 (defonce ^{:private true
            :doc "The instance of JAIN-SIP's Factories."}
@@ -41,14 +43,46 @@
   (let [s (.createSipStack ^SipFactory (:sip factories)
                            (-> (dissoc options :host :port :tcp? :udp?)
                                create-properties))
-        udp (.createListeningPoint s host port "UDP")
+        udp (.createListeningPoint s host port ListeningPoint/UDP)
         p (.createSipProvider s udp)]
-    (when tcp? (.addListeningPoint p (.createListeningPoint s host port "TCP")))
+    (when tcp? (.addListeningPoint p (.createListeningPoint s host port ListeningPoint/TCP)))
     (when (not udp?) (.removeListeningPoint p udp))
     p))
 
+(defn- get-headers [^Request request]
+  (reduce
+   (fn [headers header-name]
+     (assoc headers
+       (-> (lower-case header-name) keyword)
+       (->> (.getHeaders request header-name)
+            enumeration-seq
+            (map #(.getValue ^SIPHeader %)) ; urgly, (memfn getValue) is more better, but fire reflection warning
+            (join ", "))))
+   {}
+   (enumeration-seq (.getHeaderNames request))))
+
+(defn build-request-map [^SIPRequest request]
+  {:local-addr          (when-let [a (.getLocalAddress request)] (.getHostAddress a))
+   :local-port          (.getLocalPort request)
+   :remote-addr         (when-let [a (.getRemoteAddress request)] (.getHostAddress a))
+   :remote-port         (.getRemotePort request)
+   :method              (.getMethod request)
+   :uri                 (.getRequestURI request)
+   :headers             (get-headers request)
+   :expires             (when-let [h (.getExpires request)] (.getExpires h))
+   :content-type        (when-let [^ContentTypeHeader h (.getHeader request ContentTypeHeader/NAME)]
+                          {:type (.getContentType h), :sub-type (.getContentSubType h)})
+   :content-length      (when-let [h (.getContentLength request)] (.getContentLength h))
+   :content-encoding    (when-let [h (.getContentEncoding request)] (.getEncoding h))
+   :content-language    (when-let [h (.getContentLanguage request)] (str (.getContentLanguage h)))
+   :content-disposition (when-let [h (.getContentDisposition request)]
+                          {:type (.getDispositionType h), :handling (.getHandling h)})
+   :content             (.getRawContent request)})
+
 (defn- proxy-handler
-  [handler])
+  [handler]
+  (reify SipListener
+    (processRequest [this event])))
 
 (defn ^SipProvider run-nist
   "Start a nist SIP stack to serve the given handler according
--- sing-nist-adapter/test/sing/adapter/test/nist.clj
+++ sing-nist-adapter/test/sing/adapter/test/nist.clj
@@ -1,13 +1,19 @@
 (ns sing.adapter.test.nist
-  (:use clojure.test
-        sing.adapter.nist))
+  (:refer-clojure :exclude [replace])
+  (:require [clojure.string :refer [upper-case]]
+            [clojure.test :refer :all]
+            [clojure.pprint :refer [pprint]]
+            [sing.adapter.nist :refer :all])
+  (:import [java.util Properties]
+           [javax.sip SipProvider ListeningPoint]
+           [gov.nist.javax.sip SipStackImpl]))
 
 (defmacro with-private-fns [[ns fns] & tests]
   "Refers private fns from ns and runs tests in context."
   `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
      ~@tests))
 
-(with-private-fns [sing.adapter.nist [kw->propname create-properties]]
+(with-private-fns [sing.adapter.nist [kw->propname create-properties create-provider]]
   (deftest test-keyword->property-name
     (testing "normal JAIN property."
       (is (= (kw->propname :property-name) "javax.sip.PROPERTY_NAME")))
@@ -21,15 +27,50 @@
            :nistproperty-name "javax.sip.NISTPROPERTY_NAME")))
   (deftest test-create-properties
     (testing "type of return value."
-      (is (instance? java.util.Properties (create-properties {:name "ring-test"}))))
+      (is (instance? Properties (create-properties {:name "sing-test"}))))
     (testing "options have name and others."
-      (is (= (create-properties {:name "ring-test", :prop-1 "prop 1", :nist-prop-2 "prop 2"})
-             (doto (java.util.Properties.)
-               (.setProperty "javax.sip.STACK_NAME" "ring-test")
+      (is (= (create-properties {:name "sing-test", :prop-1 "prop 1", :nist-prop-2 "prop 2"})
+             (doto (Properties.)
+               (.setProperty "javax.sip.STACK_NAME" "sing-test")
                (.setProperty "javax.sip.PROP_1" "prop 1")
                (.setProperty "gov.nist.javax.sip.PROP_2" "prop 2")))))
     (testing "options do not have name."
       (is (thrown? AssertionError (create-properties {:prop-1 "prop 1", :nist-prop-2 "prop 2"}))))
     (testing "values contains nil."
       (are [x] (thrown? AssertionError (create-properties x))
-           {:name nil} {:name "ring-test", :prop nil}))))
+           {:name nil} {:name "sing-test", :prop nil}))
+    (testing "numeric value auto convert to string."
+      (are [x y] (= (create-properties {:name "sing-test", :prop x})
+                    (doto (Properties.)
+                      (.setProperty "javax.sip.STACK_NAME" "sing-test")
+                      (.setProperty "javax.sip.PROP" y)))
+           12 "12"
+           123.45 "123.45")))
+  (deftest test-create-provider
+    (testing "SipProvider without tcp nor udp."
+      (is (thrown? AssertionError (create-provider {:name "sing-test" :host "127.0.0.1" :port 5060}))))
+    (testing "SipProvider with 5060 udp."
+      (let [^SipProvider p (create-provider {:name "sing-test", :host "127.0.0.1", :port 5060, :udp? true})
+            eps (.getListeningPoints p)]
+        (is (= (alength eps) 1))
+        (let [^ListeningPoint ep (aget eps 0)]
+          (is (= (upper-case (.getTransport ep)) ListeningPoint/UDP))
+          (is (= (.getPort ep) 5060))
+          (is (= (.getIPAddress ep) "127.0.0.1")))
+        (.. p (getSipStack) (stop))))
+    (testing "SipProvider with 5070 tcp."
+      (let [^SipProvider p (create-provider {:name "sing-test", :host "127.0.0.1", :port 5070, :tcp? true})
+            eps (.getListeningPoints p)]
+        (is (= (alength eps) 1))
+        (let [^ListeningPoint ep (aget eps 0)]
+          (is (= (upper-case (.getTransport ep)) ListeningPoint/TCP))
+          (is (= (.getPort ep) 5070))
+          (is (= (.getIPAddress ep) "127.0.0.1")))
+        (.. p (getSipStack) (stop))))
+    (testing "SipProvider with 5060 udp & tcp."
+      (let [^SipProvider p (create-provider {:name "sing-test", :host "127.0.0.1", :port 5060, :udp? true, :tcp? true})]
+        (is (= (alength (.getListeningPoints p)) 2))
+        (let [tcp (.getListeningPoint p ListeningPoint/TCP)
+              udp (.getListeningPoint p ListeningPoint/UDP)]
+          (is (not (or (nil? tcp) (nil? udp)))))
+        (.. p (getSipStack) (stop))))))
